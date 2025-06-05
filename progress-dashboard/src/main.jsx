5a50886a8e354b9b43e4dc47791fc2dc4723643f
matthewmurawski2 <matthew.murawski2@gmail.com>
2025-06-04 21:20:27 -0400

    added main

---
+++ progress-dashboard/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Dashboard from "./Dashboard.jsx";
+import "./style.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Dashboard />);
